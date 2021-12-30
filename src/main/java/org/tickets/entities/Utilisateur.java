a111362c7899c885dcd77a001dfba566497e42b1
abdelhafidel <hafid8live@hotmail.fr>
2021-12-30 20:50:25 +0100

    adding users entitys

---
+++ src/main/java/org/tickets/entities/Utilisateur.java
@@ -0,0 +1,99 @@
+package org.tickets.entities;
+
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "user_type", discriminatorType = DiscriminatorType.STRING)
+public class Utilisateur {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id_utilisateur;
+	private String nom;
+	private String prenom;
+	private String mail;
+	private String telephone;
+	private String login;
+	private String password;
+
+	public Utilisateur(Long id_utilisateur, String nom, String prenom, String mail, String telephone, String login,
+			String password) {
+		super();
+		this.id_utilisateur = id_utilisateur;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.mail = mail;
+		this.telephone = telephone;
+		this.login = login;
+		this.password = password;
+	}
+
+	public Utilisateur() {
+		super();
+	}
+
+	public Long getId_utilisateur() {
+		return id_utilisateur;
+	}
+
+	public void setId_utilisateur(Long id_utilisateur) {
+		this.id_utilisateur = id_utilisateur;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
