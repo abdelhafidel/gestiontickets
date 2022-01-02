fa1c674afb5f98d55a9c8358135de051534c1b52
imane_jeblaoui <jeblaoui.imane@gmail.com>
2022-01-02 15:54:09 +0100

    Ticket and Domaine Controllers

--- src/main/java/org/tickets/services/DomaineService.java
+++ src/main/java/org/tickets/services/DomaineService.java
@@ -1,5 +1,40 @@
 package org.tickets.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.tickets.dao.DaoDomain;
+import org.tickets.entities.Domaine;
+
+@Service
 public class DomaineService {
 
+	@Autowired
+	private DaoDomain daoDomain;
+
+	public Domaine save(Domaine entity) {
+		return daoDomain.save(entity);
+	}
+
+	public List<Domaine> findAll() {
+		return daoDomain.findAll();
+	}
+
+	public Domaine findById(Long id) {
+		return daoDomain.findById(id).get();
+	}
+
+	public void deleteById(Long id) {
+		daoDomain.deleteById(id);
+	}
+
+	public Domaine getById(Long id) {
+		return daoDomain.getById(id);
+	}
+	
+	
+
+	
 }
